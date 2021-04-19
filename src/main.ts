ef535c652e4aad46cc1b332207153e2952a4ac3e
Semyon Levin <levin.semen@gmail.com>
2021-04-19 19:23:28 +0300

    Draft implementation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,41 @@
 import * as core from '@actions/core'
+import * as fs from 'fs'
+import * as javaProps from 'java-props'
+import path from 'path'
+import workspacePath from './internal/workspacePath'
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 async function run(): Promise<void> {
     try {
-        core.info('action logic')
+        const file = core.getInput('file', {required: true})
+        const failIfFileNotFound = core.getInput('failIfFileNotFound').toLowerCase() === 'true'
+        const property = core.getInput('property', {required: true})
+        const failIfPropertyNotFound = core.getInput('failIfFileNotFound').toLowerCase() === 'true'
+        const defaultValue = core.getInput('defaultValue')
+
+        const absoluteFile = path.resolve(workspacePath, file)
+        if (!fs.existsSync(absoluteFile)) {
+            if (failIfFileNotFound) {
+                throw new Error(`Properties file can't be found: ${absoluteFile}`)
+            } else {
+                core.setOutput('result', defaultValue)
+                return
+            }
+        }
+
+        const properties = await javaProps.parseFile(absoluteFile)
+        const value = properties[property]
+        if (value == null) {
+            if (failIfPropertyNotFound) {
+                throw new Error(`Property '${property}' can't be found in properties file: ${absoluteFile}`)
+            } else {
+                core.setOutput('result', defaultValue)
+                return
+            }
+        }
+
+        core.setOutput('result', value)
 
     } catch (error) {
         core.setFailed(error)
